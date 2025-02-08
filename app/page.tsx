b956bbc0f0efece3dffd4de5f254abd150478f6f
thanatkrit289 <thanatkrit289@gmail.com>
2025-02-08 17:20:36 +0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li>ยินดีต้อนรับเข้าสู่ Nextjs</li>
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
